8cdff3c1f74212855ce5cf68a6b02de3d165df45
Doug Sheridan <dsheridan@intraedge.com>
2016-08-08 08:58:38 -0700

    Stopped @ p 177

---
+++ node-authentication/public/app/services/authService.js
@@ -0,0 +1,86 @@
+angular.module('authService', [])
+
+.factory('Auth', function($http, $q, AuthToken) {
+    var authFactory = {};
+
+    // login
+    authFactory.login = function(username, password) {
+        return $http.post('/api/authenticate', {
+                username: username,
+                password: password
+            })
+            .success(function(data) {
+                AuthToken.setToken(data.token);
+            });
+    };
+
+    // logout
+    authFactory.logout = function() {
+        // clear the token
+        AuthToken.setToken();
+    };
+
+    // check if logged in
+    authFactory.isLoggedIn = function() {
+        if (AuthToken.getToken()) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    authFactory.getUser = function() {
+        if (AuthToken.getToken()) {
+            return $http.get('/api/me');
+        } else {
+            return $q.reject({ message: 'User has no token.' });
+        }
+    };
+
+    return authFactory;
+})
+
+.factory('AuthToken', function($window) {
+    var authTokenFactory = {};
+
+    authTokenFactory.getToken = function() {
+        return $window.localStorage.getItem('token');
+    };
+
+    authTokenFactory.setToken = function(token) {
+        if (token) {
+            $window.localStorage.setItem('token', token);
+        } else {
+            $window.localStorage.removeItem('token');
+        }
+    };
+
+    return authTokenFactory;
+})
+
+.factory('AuthInterceptor', function($q, $location, AuthToken) {
+    var interceptorFactory = {};
+
+    // Happens on all http requests
+    interceptorFactory.request = function(config) {
+        var token = AuthToken.getToken();
+
+        if (token) {
+            config.headers['x-access-token'] = token;
+        }
+
+        return config;
+    };
+
+    interceptorFactory.responseError = function(response) {
+        if (response.status == 403) {
+            AuthToken.setToken();
+            $location.path('/login');
+        }
+
+        return $q.reject(response);
+    };
+
+
+    return interceptorFactory;
+});
