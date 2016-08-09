e0f190f0bfe1dc09b2f8dca59cf96e60d898f57c
Doug Sheridan <doug.sheridan2@vantiv.com>
2016-08-08 18:02:51 -0700

    Fix db uri

--- node-authentication/config.js
+++ node-authentication/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'port': process.env.PORT || 8080,
-    'database': 'mongodb://localhost:27017/NodeAuth',
+    'database': "mongodb://admin:admin@ds145355.mlab.com:45355/heroku_kdj9nz7t",
     'secret': 'ilovescotchscotchyscotchscotch'
 };
